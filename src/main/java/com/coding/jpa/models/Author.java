688e3f7368213faf32bd2fe53c03ad67c435fe2b
aromaric <aromaricadjei213@gmail.com>
2024-12-26 11:45:41 +0000

    generations values options

--- src/main/java/com/coding/jpa/models/Author.java
+++ src/main/java/com/coding/jpa/models/Author.java
@@ -5,14 +5,16 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @Data
 @Entity
-@Table
 public class Author {
 
     @Id
-    @GeneratedValue(
+    @GeneratedValue
+            /*(
             strategy = GenerationType.TABLE,
             generator = "author_id_gen"
     )
@@ -20,14 +22,41 @@ public class Author {
             name = "author_sequence",
             sequenceName = "author_sequence",
             allocationSize = 1
+    )
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            allocationSize = 1
     )*/
+
     private Integer id;
 
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
 
     private String lastName;
 
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
 
     private int age;
+
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
 }
