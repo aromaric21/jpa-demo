a5d3217e1f435a0b499cb98cf316d2e0042e5bca
aromaric <aromaricadjei213@gmail.com>
2024-12-28 07:30:37 +0000

    Derived queries explained

--- src/main/java/com/coding/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/coding/jpa/repositories/AuthorRepository.java
@@ -3,5 +3,27 @@ package com.coding.jpa.repositories;
 import com.coding.jpa.models.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    // select * from author where first_name = 'ali'
+    List<Author> findAllByFirstName(String fn);
+
+    // select * from author where first_name = 'al'
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+
+    // select * from author where first_name like '%al%'
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+
+    // select * from author where first_name like 'al%'
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String fn);
+
+    // select * from author where first_name like '%al'
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String fn);
+
+    // select * from author where first_name in ('ali', 'bou', 'coding')
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> firstName);
+
+
 }
