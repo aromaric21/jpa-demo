2c99edea421e6fa8fc265841af03476618d66268
aromaric <aromaricadjei213@gmail.com>
2024-12-28 00:10:48 +0000

    Use the embedded ID

--- src/main/java/com/coding/jpa/JpaApplication.java
+++ src/main/java/com/coding/jpa/JpaApplication.java
@@ -1,13 +1,43 @@
 package com.coding.jpa;
 
 
+import com.coding.jpa.models.Author;
+import com.coding.jpa.models.Video;
+import com.coding.jpa.repositories.AuthorRepository;
+import com.coding.jpa.repositories.VideoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthorRepository repository,
+			VideoRepository videoRepository
+	){
+		return args -> {
+
+			/*var author = Author.builder()
+					.firstName("alibou")
+					.lastName("alibou")
+					.age(34)
+					.email("conatact@aliboucoding.com")
+					.build();
+			repository.save(author); */
+
+			var video = Video.builder()
+					.name("ABC")
+					.length(5)
+					.size(10)
+					.build();
+			videoRepository.save(video);
+		};
 	}
 }
